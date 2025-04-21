08b11c2919f4d936effbc07cbb1e28ebf7508c4e
root <root@ip-10-0-0-32.ap-south-2.compute.internal>
2025-04-21 09:51:20 +0000

    push day4

--- TerraformDay4/main.tf
+++ TerraformDay4/main.tf
@@ -21,3 +21,11 @@ resource "aws_subnet" "subnet_test" {
   }
 
 }
+resource "aws_subnet" "subnet_prod" {
+  vpc_id     = aws_vpc.VPC.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "Prod_Subnet"
+  }
+
+}
