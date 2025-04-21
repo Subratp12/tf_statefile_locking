7dad5ee692de5c59662f73840f80bd9a9f1637f8
unknown <subrat.padhi1204@outlook.com>
2025-04-21 16:00:33 +0530

    final commit1

--- TerraformDay4/main.tf
+++ TerraformDay4/main.tf
@@ -19,12 +19,14 @@ resource "aws_subnet" "subnet_test" {
   tags = {
     Name = "Test_Subnet"
   }
+}
 resource "aws_subnet" "subnet_UAT" {
     vpc_id = aws_vpc.VPC.id
     cidr_block = "10.0.4.0/24"
     tags = {
         Name = "UAT_Subnet"
     }
+}
     resource "aws_subnet" "subnet_prod" {
   vpc_id     = aws_vpc.VPC.id
   cidr_block = "10.0.2.0/24"
