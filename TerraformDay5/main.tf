8a525d090345d3e3ac05e70e6845eec0757e9516
unknown <subrat.padhi1204@outlook.com>
2025-04-22 09:47:21 +0530

    Day5-commit-2

--- TerraformDay5/main.tf
+++ TerraformDay5/main.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-05cef57fad40b2755"
   instance_type = "t3.micro"
   key_name      = "AWS_hyd"
-  
+  tags = {
+    Name        = "MyEC2Instance"
+  }
 }
