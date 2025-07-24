c8b77880512908008564cafc458c6904b6d237bd
unknown <subrat.padhi1204@outlook.com>
2025-07-24 17:08:11 +0530

    Added RDS instance with subnet group fix

--- tf_statefile_locking/rds.tf
+++ tf_statefile_locking/rds.tf
@@ -1,11 +1,26 @@
 resource "aws_db_subnet_group" "subrat" {
   name       = "subrat-subnet-group"
-  subnet_ids = [aws_subnet.himansu_subnet.id]
+  subnet_ids = [
+    aws_subnet.himansu_subnet.id,
+    aws_subnet.himansu_subnet_2.id
+  ]
 
   tags = {
     Name = "subrat-db-subnet-group"
   }
 }
+
+resource "aws_subnet" "himansu_subnet_2" {
+  vpc_id                  = aws_vpc.subrat_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subrat-subnet_2"
+  }
+}
+
 
 resource "aws_db_instance" "subrat_rds" {
   allocated_storage    = 20
@@ -16,7 +31,7 @@ resource "aws_db_instance" "subrat_rds" {
   username             = var.db_username
   password             = var.db_password
   skip_final_snapshot  = true
-  publicly_accessible  = true
+  publicly_accessible  = false
   db_subnet_group_name = aws_db_subnet_group.subrat.name  # ✅ Direct reference
 
   tags = {
